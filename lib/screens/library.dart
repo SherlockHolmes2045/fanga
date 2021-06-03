c92613afbe618cf8eb09c538cde200f4b490cb6b
SherlockHolmes2045 <lemovou@gmail.com>
2021-06-03 05:44:51 +0100

    use mangaitem in library and started logic for filtering

--- lib/screens/library.dart
+++ lib/screens/library.dart
@@ -1,10 +1,10 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:manga_reader/constants/assets.dart';
 import 'package:manga_reader/custom/widgets/app_drawer.dart';
+import 'package:manga_reader/custom/widgets/error.dart';
 import 'package:manga_reader/custom/widgets/library_search_delegate.dart';
+import 'package:manga_reader/custom/widgets/manga_item.dart';
 import 'package:manga_reader/custom/widgets/scale_route_transition.dart';
-import 'package:manga_reader/database/dao/download_dao.dart';
 import 'package:manga_reader/screens/readmangatoday/readmangatoday_manga_details.dart';
 import 'package:manga_reader/state/LoadingState.dart';
 import 'package:manga_reader/state/lelscan/lelscan_provider.dart';
@@ -72,12 +72,7 @@ class _LibraryState extends State<Library> {
                   .library
                   .fold(
                   (NException error) {
-                    return Center(
-                      child: Text(
-                        error.message,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    );
+                    return Error(reload: context.read<LibraryProvider>().loadLibrary(), error: error);
                   },
                   (mangaList) {
                     return mangaList.isEmpty
@@ -123,117 +118,39 @@ class _LibraryState extends State<Library> {
                                 SizeConfig.blockSizeHorizontal * 2,
                             mainAxisSpacing: SizeConfig.blockSizeVertical,
                             children: List.generate(mangaList.length, (index) {
-                              return Container(
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.max,
-                                  children: [
-                                    Flexible(
-                                      child: GestureDetector(
-                                        onTap: () {
-                                          print(mangaList[index].catalog);
-                                          switch (mangaList[index].catalog) {
-                                            case Assets.lelscanCatalogName:
-                                              {
-                                                Navigator.push(
-                                                    context,
-                                                    ScaleRoute(
-                                                        page: LelscanDetail(
-                                                      manga: mangaList[index],
-                                                    )));
-                                              }
-                                              break;
+                              return MangaItem(detailsNavigation: () {
+                                switch (mangaList[index].catalog) {
+                                  case Assets.lelscanCatalogName:
+                                    {
+                                      Navigator.push(
+                                          context,
+                                          ScaleRoute(
+                                              page: LelscanDetail(
+                                                manga: mangaList[index],
+                                              )));
+                                    }
+                                    break;
 
-                                            case Assets
-                                                .readmangatodayCatalogName:
-                                              {
-                                                Navigator.push(
-                                                    context,
-                                                    ScaleRoute(
-                                                        page:
-                                                            ReadmangatodayDetail(
-                                                      manga: mangaList[index],
-                                                    )));
-                                              }
-                                              break;
+                                  case Assets
+                                      .readmangatodayCatalogName:
+                                    {
+                                      Navigator.push(
+                                          context,
+                                          ScaleRoute(
+                                              page:
+                                              ReadmangatodayDetail(
+                                                manga: mangaList[index],
+                                              )));
+                                    }
+                                    break;
 
-                                            default:
-                                              {
-                                                //statements;
-                                              }
-                                              break;
-                                          }
-                                        },
-                                        child: CachedNetworkImage(
-                                          imageUrl:
-                                              mangaList[index].thumbnailUrl,
-                                          width: double.infinity,
-                                          height: 350,
-                                          errorWidget: (context, text, data) {
-                                            return GestureDetector(
-                                              onTap: () {
-                                                switch (
-                                                    mangaList[index].catalog) {
-                                                  case Assets
-                                                      .lelscanCatalogName:
-                                                    {
-                                                      Navigator.push(
-                                                          context,
-                                                          ScaleRoute(
-                                                              page:
-                                                                  LelscanDetail(
-                                                            manga: mangaList[
-                                                                index],
-                                                          )));
-                                                    }
-                                                    break;
-
-                                                  case Assets
-                                                      .readmangatodayCatalogName:
-                                                    {
-                                                      Navigator.push(
-                                                          context,
-                                                          ScaleRoute(
-                                                              page:
-                                                                  ReadmangatodayDetail(
-                                                            manga: mangaList[
-                                                                index],
-                                                          )));
-                                                    }
-                                                    break;
-
-                                                  default:
-                                                    {
-                                                      //statements;
-                                                    }
-                                                    break;
-                                                }
-                                              },
-                                              child: Image.asset(
-                                                Assets.errorImage,
-                                                width: double.infinity,
-                                                height: 350,
-                                              ),
-                                            );
-                                          },
-                                          //fit: BoxFit.fill,
-                                        ),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsets.only(
-                                          top: SizeConfig.blockSizeVertical),
-                                      child: Text(
-                                        mangaList[index].title,
-                                        overflow: TextOverflow.clip,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                        ),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              );
+                                  default:
+                                    {
+                                      //statements;
+                                    }
+                                    break;
+                                }
+                              }, addLibrary: (){}, libraryList: [], manga:mangaList[index]);
                             }),
                           );
                   },
--- lib/state/lelscan/chapter_provider.dart
+++ lib/state/lelscan/chapter_provider.dart
@@ -1,4 +1,7 @@
+import 'dart:collection';
+
 import 'package:dartz/dartz.dart';
+import 'package:manga_reader/database/dao/page_dao.dart';
 import 'package:manga_reader/models/chapter.dart';
 import 'package:manga_reader/models/manga.dart';
 import 'package:manga_reader/networking/services/lelscan_service.dart';
@@ -9,15 +12,25 @@ class ChapterProvider extends BaseProvider {
   Either<NException,List<Chapter>> mangaChapters = Right([]);
   bool isFiltered = false;
   List<Chapter> filteredChapters = List<Chapter>();
+  PageDao pageDao = PageDao();
 
   bool downloaded = false;
   bool nonreaded = false;
   bool readed = false;
   bool marked = false;
 
+
+
   filterDownloaded(bool value){
-    downloaded = value;
-    notifyListeners();
+    if(value){
+      isFiltered = true;
+      downloaded = value;
+      notifyListeners();
+    }else{
+      isFiltered = false;
+      downloaded = value;
+      notifyListeners();
+    }
   }
   filterNonReaded(bool value){
     nonreaded = value;
@@ -25,15 +38,41 @@ class ChapterProvider extends BaseProvider {
   }
 
   filterReaded(bool value){
-    readed = value;
-    notifyListeners();
+    if(value) {
+      isFiltered = true;
+      readed = value;
+      pageDao.getAll().then((value) {
+        //List<SomeClass> list = list to search;
+        List<Chapter> matchingList = value.map((page) => page.chapter).toList();
+
+        final matchingSet = HashSet.from(matchingList);
+        mangaChapters.fold((l) => null, (r){
+          final result = r.where((item) => matchingSet.contains(item));
+          filteredChapters.addAll(result);
+        });
+        notifyListeners();
+      });
+    }else{
+      isFiltered = false;
+      filteredChapters.clear();
+      readed = value;
+      notifyListeners();
+    }
+
   }
 
   filterMarked(bool value){
     marked = value;
     notifyListeners();
   }
-
+  clearAllFilters(){
+    isFiltered = false;
+    downloaded = false;
+    readed = false;
+    nonreaded = false;
+    marked = false;
+    notifyListeners();
+  }
 
   getChapters(String catalogName,Manga manga,bool forceRefresh){
     this.toggleLoadingState();
