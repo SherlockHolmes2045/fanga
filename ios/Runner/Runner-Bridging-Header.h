36bc781fe83a8b382c83cb074766b8382792d739
Ivan Lemovou <lemovou@gmail.com>
2021-10-28 21:52:33 +0100

    fixed all issues with conflicting files on ios

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 #import "GeneratedPluginRegistrant.h"
-=======
-#import "GeneratedPluginRegistrant.h"
->>>>>>> 6fa6b0b (changed app package name and fix issue with downloading and removed some useless lines of code)
